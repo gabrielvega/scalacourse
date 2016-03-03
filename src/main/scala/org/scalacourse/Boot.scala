12f986c6da2929515b216e397cf2bc339d2ca425
Gabriel Vega <gabriel.vega@synergy-gb.com>
2016-03-03 12:05:17 -0430

    fixing context definition val por def

--- src/main/scala/org/scalacourse/Boot.scala
+++ src/main/scala/org/scalacourse/Boot.scala
@@ -4,31 +4,34 @@ import com.synergygb.zordon.common.data.DataTransformContext
 import com.synergygb.zordon.core.ServiceBoot
 import com.synergygb.zordon.gen.models.Project
 import com.synergygb.zordon.gen.routes.ApplicationRoutesConsolidated
+import org.scalacourse.Models.ProjectResponse
 import spray.routing.Route
 
 object Boot extends App with ServiceBoot with ApplicationRoutesConsolidated {
 
   import Context.keyValueStore
-  implicit val context = Context
+  implicit def context = Context
 
   override def dataContext: DataTransformContext = context
+
   protected def apiResourceClass = getClass
 
   override def handlePostProject(create: Project)(): Route = {
     keyValueStore.write("gvproject" + create.id, create)
-    complete("Project saved")
+    complete(ProjectResponse("Project saved"))
 
   }
 
   override def handlePutProjectId(id: String, create: Project)(): Route = {
     keyValueStore.write("gvproject" + id, create)
-    complete("Project updated")
+    complete(ProjectResponse("Project updated"))
+
 
   }
 
   override def handleDeleteProjectId(id: String)(): Route = { id =>
     keyValueStore.delete("gvproject" + id)
-    complete("OK")
+    complete(ProjectResponse("Project Deleted"))
   }
 
 }
--- src/main/scala/org/scalacourse/Models.scala
+++ src/main/scala/org/scalacourse/Models.scala
@@ -3,11 +3,11 @@ package org.scalacourse
 import spray.json.DefaultJsonProtocol._
 
 object Models {
-/*
-  implicit val projectFormat = jsonFormat6(Project)
-  implicit val taskFormat = jsonFormat3(Task)
 
-  case class Project(id: Int, name : String, leader: String, start: String, finish: String, task: Seq)
-  case class Task(name : String, user: String, status: String)
-  */
+  implicit val projectFormat = jsonFormat1(ProjectResponse)
+  //implicit val taskFormat = jsonFormat3(Task)
+
+  case class ProjectResponse(name: String)
+  //case class Task(name : String, user: String, status: String)
+
 }
