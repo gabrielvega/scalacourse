1d53b87729ea2b3a76e11c5a629029659cfe1336
Gabriel Vega <gabriel.vega@synergy-gb.com>
2016-03-03 11:45:03 -0430

    first commit

--- src/main/scala/org/scalacourse/Boot.scala
+++ src/main/scala/org/scalacourse/Boot.scala
@@ -1,12 +1,34 @@
 package org.scalacourse
 
+import com.synergygb.zordon.common.data.DataTransformContext
 import com.synergygb.zordon.core.ServiceBoot
+import com.synergygb.zordon.gen.models.Project
 import com.synergygb.zordon.gen.routes.ApplicationRoutesConsolidated
+import spray.routing.Route
 
 object Boot extends App with ServiceBoot with ApplicationRoutesConsolidated {
 
+  import Context.keyValueStore
   implicit val context = Context
 
+  override def dataContext: DataTransformContext = context
   protected def apiResourceClass = getClass
+
+  override def handlePostProject(create: Project)(): Route = {
+    keyValueStore.write("gvproject" + create.id, create)
+    complete("Project saved")
+
+  }
+
+  override def handlePutProjectId(id: String, create: Project)(): Route = {
+    keyValueStore.write("gvproject" + id, create)
+    complete("Project updated")
+
+  }
+
+  override def handleDeleteProjectId(id: String)(): Route = { id =>
+    keyValueStore.delete("gvproject" + id)
+    complete("OK")
+  }
 
 }
---
+++ src/main/scala/org/scalacourse/Models.scala
@@ -0,0 +1,13 @@
+package org.scalacourse
+
+import spray.json.DefaultJsonProtocol._
+
+object Models {
+/*
+  implicit val projectFormat = jsonFormat6(Project)
+  implicit val taskFormat = jsonFormat3(Task)
+
+  case class Project(id: Int, name : String, leader: String, start: String, finish: String, task: Seq)
+  case class Task(name : String, user: String, status: String)
+  */
+}
